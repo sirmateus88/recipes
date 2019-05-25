c15fc243aa60f20230859888e1d1996353855f9a
Matthew Appleby <mappleby1@me.com>
2019-05-24 23:29:41 -0400

    restructured to use a different API

--- server/api/parse.js
+++ server/api/parse.js
@@ -1,75 +1,22 @@
 const router = require('express').Router()
 const axios = require('axios');
-const Nightmare = require('nightmare');
-const nightmare = Nightmare({executionTimeout: 10000});
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const htmlparser = require('htmlparser');
 
-const handler = new htmlparser.DefaultHandler(function (error, dom) {
-  if (error)
-      console.log('there was a problem, ', error);
-  else
-      console.log('parse complete bro!');
-});
-
-const parser = new htmlparser.Parser(handler);
-
-const findIngredients = nodes => {
-  let currNode = {};
-  //console.log('I\'m in it and I can\'t get out', nodes)
-  for (let i = 0; i < nodes.length; i++){
-    let found = {};
-    try {
-      currNode = nodes[i]
-      console.log('\n\n\n\n\n\nthe current node brah ', currNode);
-
-      if (currNode.type === 'text'){
-        console.log('\n--------------\n currNode.data', currNode.data, 'currNode Type', currNode.type);
-        let whereIngredient = currNode.data.toLowerCase().indexOf('ingredient');
-        if (whereIngredient !== -1 && whereIngredient < 5){
-          console.log('GOT IT', currNode)
-          return currNode;
-        }
-      } else if (currNode.children) {
-        found = findIngredients(currNode.children)
-        if (Object.keys(found).length > 0){
-          console.log('in the else brah', Object.keys(found).length)
-          console.log('found', found)
-          return found;
-        }
-      }
-
-    } catch (err){
-      console.error(err);
-    }
-
+const getFromFoodAPI = axios.create({
+  baseURL: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/',
+  headers: {
+    'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.SPOONACULAR_API_KEY
   }
-  return {};
-}
+})
 
-const getIngredientList = node => {
-  let currNode = node;
-  if (currNode.type === 'tag' && currNode.data === 'ul'){
-    console.log('found the tag');
-  }
-}
 
-router.get('/recipe', (req, res, next) => {
-  //console.log('request query: ', req.query.url)
+router.get('/', (req, res, next) => {
+  console.log('request query: ', req.query.url)
 
-  axios.get(req.query.url)
+  getFromFoodAPI.get(`recipes/extract?url=${req.query.url}`)
   .then(response => response.data)
   .then(data => {
-    parser.parseComplete(data)
-    //console.log('yea parser', parser);
-    //console.log('yea handler', handler.dom);
-    return handler.dom
-  })
-  .then(parsedDom => {
-    let found = findIngredients(parsedDom)
-    console.log('DID WE FIND IT????', found);
-    res.send(parsedDom);
+    res.status(200).send(data)
   })
   .catch(err => {
     res.status(500).send(err)
@@ -77,103 +24,5 @@ router.get('/recipe', (req, res, next) => {
 
 })
 
-// router.get('/recipe', (req, res, next) => {
-//   console.log('request query: ', req.query.url)
-//   let p1 = 1
-//   let p2 = 3
-//   nightmare
-//   .goto(req.query.url)
-//   .evaluate(p1 => {
-//     return {
-//       key: document.body
-//     }
-//   }, p1) // <-- that's how you pass parameters from Node scope to browser scope
-//  //end evaluate
-//   .then(body => {
-//     console.log('panic')
-//     findIngredients(body)
-//   })
-//   .then(result => {
-//     res.send(result);
-//   })
-//   .catch(err => {
-//     res.send("Help something went wrong out heah")
-//     console.error(err)});
-// })
-
-// router.get('/', (req, res, next) => {
-//   axios.get(`https://smittenkitchen.com/2016/10/pumpkin-bread/`)
-//     .then(found => found.data)
-//     .then(data => parser.parseComplete(data))
-//     .then(_ => {
-//       res.json(parser._handler.dom[2].attribs)
-//     })
-//     .catch(err => res.json(err))
-// })
-
-// router.get('/', (req, res, next) => {
-//   axios.get(`https://smittenkitchen.com/2016/10/pumpkin-bread/`)
-//     .then(found => found.data)
-//     .then(data => findInredients(data))
-//     .then(_ => res.send(found))
-//     .catch(err => res.json(err))
-// })
 
 module.exports = router
-
-
-const pullItem = ingredient => {
-  let start = ingredient.search('>');
-  let trimmed = ingredient.slice(start + 1);
-  return trimmed;
-}
-
-// const findInredients = content => {
-//   let ingredientPos = content.search('ingredient')
-//   let trimmedIngredients = content.slice(ingredientPos);
-//   let startOfList = trimmedIngredients.search('<ul');
-//   let potentialIngredients = trimmedIngredients.slice(startOfList + 4)
-//   let endOfList = potentialIngredients.search('</ul>')
-//   potentialIngredients = potentialIngredients.slice(0, endOfList);
-//   let ingreArr = potentialIngredients.split('</li>');
-//   return ingreArr.map(ingredient => pullItem(ingredient)).filter(item => item !== '');
-// }
-
-
-
-const findRecipie = content => {
-  let recipiePos = content.search('jetpack-recipe-directions')
-  let trimmedRecipie = content.slice(recipiePos);
-  let startOfList = trimmedRecipie.search('>');
-  let potentialRecipie = trimmedRecipie.slice(startOfList + 1)
-  let endOfList = potentialRecipie.search('</div>')
-  potentialRecipie = potentialRecipie.slice(0, endOfList);
-  let recipieArr = potentialRecipie.split('</p>');
-  return recipieArr.map(recipe => pullItem(recipe)).filter(item => item !== '');
-}
-
-
-// router.get('/', (req, res, next) => {
-//   console.log('request query: ', req.query.url)
-//   axios({
-//     method: 'get',
-//     'Content-Type': 'application/json',
-//     url: `https://mercury.postlight.com/parser?url=${req.query.url}}`,
-//     headers: {'x-api-key': process.env.MERCURY_API_KEY}
-//   })
-//     .then(found => {
-//       console.log('found: ', found.data)
-//       let title = found.data.title
-//       let ingredients = findInredients(found.data.content)
-//       let recipie = findRecipie(found.data.content)
-//       return {
-//         title,
-//         ingredients,
-//         recipie
-//       }
-//     })
-//     .then(processed => res.json(processed))
-//     .catch(err => {
-//       res.send("There was a problem connecting to the server")
-//       console.error(err)});
-// })
